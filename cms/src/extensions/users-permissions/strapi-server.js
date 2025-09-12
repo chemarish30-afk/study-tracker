56e5ab763d89c784fc63ca55bde3885f219cbfd1
bala <bala@example.com>
2025-09-12 18:45:14 +0530

    fix: Remove custom forgot password extension to use Strapi native functionality
    - Remove custom forgotPassword controller that was causing 500 errors
    - Let Strapi handle forgot password natively with configured email service
    - Fix reset password URL in email template
    - Allow Strapi's built-in email templates to work properly
    - This should resolve the 500 Internal Server Error in forgot password

--- cms/src/extensions/users-permissions/strapi-server.js
+++ cms/src/extensions/users-permissions/strapi-server.js
@@ -45,44 +45,7 @@ module.exports = (plugin) => {
     ctx.send({ message: 'Confirmation email sent' });
   };
 
-  plugin.controllers.auth.forgotPassword = async (ctx) => {
-    const { email } = ctx.request.body;
-    
-    if (!email) {
-      return ctx.badRequest('Email is required');
-    }
-
-    const user = await strapi.query('plugin::users-permissions.user').findOne({
-      where: { email },
-    });
-
-    if (!user) {
-      return ctx.badRequest('User not found');
-    }
-
-    const resetPasswordToken = strapi.plugins['users-permissions'].services.user.generateResetPasswordToken(user);
-    
-    await strapi.plugins['users-permissions'].services.user.edit(user.id, {
-      resetPasswordToken,
-    });
-
-    // Send email using the configured email provider
-    await strapi.plugins.email.services.email.send({
-      to: email,
-      from: process.env.RESEND_FROM_EMAIL,
-      subject: 'Reset your password',
-      html: `
-        <h1>Password Reset Request</h1>
-        <p>You requested a password reset. Click the link below to reset your password:</p>
-        <a href="${process.env.FRONTEND_URL || 'http://localhost:3000'}/auth/reset-password?code=${resetPasswordToken}">
-          Reset Password
-        </a>
-        <p>If you didn't request this, please ignore this email.</p>
-      `,
-    });
-
-    ctx.send({ message: 'Password reset email sent' });
-  };
+  // Let Strapi handle forgot password natively with the configured email service
 
   return plugin;
 };
